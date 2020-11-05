7fd1f99a2808ac1ffc1a9ffa6aa68e0e26ba06c1
ogadra <ogaryu0529@gmail.com>
2020-11-06 00:30:59 +0900

    add week function

--- background.js
+++ background.js
@@ -1,15 +1,47 @@
 chrome.runtime.onInstalled.addListener(function(){
 	chrome.contextMenus.create({
-	  id: "GCE",
+	  id: "hideTitleM",
 	  type: "normal",
 	  contexts: ['all'],
-	  "title": "GCE"
+	  "title": "予定を隠す(月)"
 	});
+	chrome.contextMenus.create({
+		id: "hideReminderM",
+		type: "normal",
+		contexts: ['all'],
+		"title": "リマインダーを隠す(月)"
+	});
+	chrome.contextMenus.create({
+		id: "hideTaskM",
+		type: "normal",
+		contexts: ['all'],
+		"title": "タスクを隠す(月)",
+	});
+	chrome.contextMenus.create({
+		id: "hideTitleW",
+		type: "normal",
+		contexts: ['all'],
+		"title": "予定を隠す(週)"
+	  });
+	  chrome.contextMenus.create({
+		  id: "hideReminderW",
+		  type: "normal",
+		  contexts: ['all'],
+		  "title": "リマインダーを隠す(週)"
+	  });
+	  chrome.contextMenus.create({
+		  id: "hideTaskW",
+		  type: "normal",
+		  contexts: ['all'],
+		  "title": "タスクを隠す(週)",
+	  });
   });
-  // クリック時の挙動を設定
-  chrome.contextMenus.onClicked.addListener(function(){
+
+  chrome.contextMenus.onClicked.addListener(function(info, tab){
+	const selectedMenu = info.menuItemId;
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {message: "GCE"}, function(response) {
+	  chrome.tabs.sendMessage(tabs[0].id, {message: selectedMenu}, function(response) {
 	  });
 	});
   });
+
--- script.js
+++ script.js
@@ -1,13 +1,82 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-	  if (request.message == "GCE") {
-		document.designMode = "on";
-		var elements = document.getElementsByClassName("yzifAd");
-		for (let i=0; i < elements.length; i++) {
-			console.log(elements[i]);
-			elements[i].innerHTML = "";
-			console.log(elements[i].innerHTML);
-		};
-	  }
+		if (request.message == "hideTitleM" || request.message == "hideReminderM" || request.message == "hideTaskM"){
+			// 月ビューの場合
+			let elements = document.getElementsByClassName("ifwtOb");
+			for (let i=0; i < elements.length; i++) {
+	
+				try{
+					let time = elements[i].getElementsByClassName("ynRLnc")[0].innerHTML;
+					if (request.message == "hideTitleM"){
+						if (time.match(/^\d{2}:\d{2}～\d{2}:\d{2}/)){
+							elements[i].getElementsByClassName("yzifAd")[0].innerHTML = time.match(/^\d{2}:\d{2}～\d{2}:\d{2}/);;
+							elements[i].getElementsByClassName("J0Jt0d")[0].innerHTML = "";
+						} else if (time.match("リマインダ")){
+							elements[i].getElementsByClassName("yzifAd")[0].innerHTML = "リマインダー";
+						} else if (time.match("タスク")){
+							elements[i].getElementsByClassName("yzifAd")[0].innerHTML = "タスク";
+						} else if (time.match("祝日")){
+							;
+						} else {
+							elements[i].getElementsByClassName("yzifAd")[0].innerHTML = "終日の予定";
+						}
+						
+					} else if (request.message == "hideReminderM") {
+						
+						if (time.match("リマインダ")){
+							elements[i].innerHTML = "";
+						}
+					} else {
+						if (time.match("タスク")){
+							elements[i].innerHTML = "";
+						}
+					}
+				} catch(e){
+					;
+				}
+			}
+		} else { // 週ビューの場合
+			let elements = document.getElementsByClassName("NlL62b");
+			for (let i=0; i < elements.length; i++) {
+				try {
+					var time = elements[i].getElementsByClassName("ynRLnc")[0].innerHTML;
+				} catch(e){;}
+				
+				// classList = ["FAxxKc", "K9QN7e", "yzifAd"]
+				// 予定（リマインダ、タスク）タイトル、予定詳細、終日タイトル
+				if (time.match("祝日")){
+					;
+				} else {
+					if (request.message == "hideTitleW"){
+						if (time.match("リマインダ")){
+							try{
+								elements[i].getElementsByClassName("FAxxKc")[0].innerHTML = "リマインダー";
+							}catch(e){
+								elements[i].getElementsByClassName("yzifAd")[0].innerHTML = "リマインダー";
+							}
+						} else if (time.match("タスク")){
+							elements[i].getElementsByClassName("FAxxKc")[0].innerHTML = "タスク";
+						} else {
+							try{
+								elements[i].getElementsByClassName("FAxxKc")[0].innerHTML = "";
+								try {
+									elements[i].getElementsByClassName("K9QN7e")[0].innerHTML = "";		
+								}catch(e){;}
+							}catch(e){
+								elements[i].getElementsByClassName("yzifAd")[0].innerHTML = "終日の予定";
+							}
+						}
+					} else if (request.message == "hideReminderW") {
+						if (time.match("リマインダ")){
+							elements[i].parentElement.removeChild(elements[i]);
+						}
+					} else {
+						if (time.match("タスク")){
+							elements[i].parentElement.removeChild(elements[i]);
+						}
+					}
+				}
+			}
+		}
 	}
-  );
+);
